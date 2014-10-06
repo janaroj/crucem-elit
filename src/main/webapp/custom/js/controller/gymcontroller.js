30a28c04e25765f383f107180c2442c71e0ec2f0
Janar Ojalaid <janarojalaid@gmail.com>
2014-10-06 21:44:44 +0300

    Issue 9

--- src/main/webapp/custom/js/controller/gymcontroller.js
+++ src/main/webapp/custom/js/controller/gymcontroller.js
@@ -1,8 +1,28 @@
 (function() {
 	var app = angular.module('crucem-elit');
 	
-	app.controller('GymsController', function($scope, $location, gymService) {
-		
+	app.controller('GymsController', function($scope, $location, $filter, gymService, ngTableParams) {
+		var gymData = null;
+		 $scope.tableParams = new ngTableParams({
+		        page: 1,            // show first page
+		        count: 10,          // count per page
+		        sorting: {
+		        }
+		    }, {
+		        total: 0,           // length of data
+		        getData: function($defer, params) {
+		        	if (gymData===null) {
+						gymService.getGyms($scope.tableParams, $defer).then(function(data) {
+							gymData = data;
+							ui.util.table.prepareData($defer, $filter, params, gymData);
+						});
+					}
+					else {
+						ui.util.table.prepareData($defer, $filter, params,gymData);
+						}
+		        }
+		    });
+		    
 	});
 
 
